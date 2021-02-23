3d7d955d9280dc17da5d24805d8ca6750e0f2831
Jon Marien <nuclearsmilz@gmail.com>
2021-02-23 05:40:51 -0500

    first push

---
+++ src/main/java/com/timelord/kush/Kush.java
@@ -0,0 +1,11 @@
+package com.timelord.kush;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kush implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
