97c2c5130be7947a64a2dc6a947a79aafd64b745
Jon Marien <nuclearsmilz@gmail.com>
2021-02-23 05:49:45 -0500

    Added in a "bud" basic item - no texture

--- src/main/java/com/timelord/kush/Kush.java
+++ src/main/java/com/timelord/kush/Kush.java
@@ -1,11 +1,14 @@
 package com.timelord.kush;
 
+import com.timelord.kush.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Kush implements ModInitializer {
 
+    public static final String MOD_ID = "kush";
+
     @Override
     public void onInitialize() {
-
+        ModItems.registerItems();
     }
 }
---
+++ src/main/java/com/timelord/kush/registry/Bud.java
@@ -0,0 +1,9 @@
+package com.timelord.kush.registry;
+
+import net.minecraft.item.Item;
+
+public class Bud extends Item {
+    public Bud(Settings settings) {
+        super(settings);
+    }
+}
